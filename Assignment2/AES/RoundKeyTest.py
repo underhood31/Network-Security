dd5f01ab8049b3bc5d0df017336df84bf6ed2df0
underhood31 <manavjeet18295@iiitd.ac.in>
2020-10-05 19:09:28 +0530

    corrected round key generation test

--- Assignment2/AES/RoundKeyTest.py
+++ Assignment2/AES/RoundKeyTest.py
@@ -2,21 +2,23 @@ import AES_Encryptor
 import genRandKey
 import numpy as np
 
-
-def print_matrix(roundKey):
+def print_matrix(roundKey,i):
 	mat=np.zeros([4,4],dtype='object')
-
+	r=0
 	for j in range(len(roundKey)):
 		n=roundKey[j].to01()
-		for i in range(0,32,8):
-			mat[j][i//8]=hex(int(n[i:i+8],2))
+		print(r+4*i,":",hex(int(n,2))[2:])
+		r+=1
+		# for i in range(0,32,8):
+		# 	mat[j][i//8]=
 
-	print(mat)
+	# print(mat)
 
 if __name__=="__main__":
 	# kg=genRandKey.GenerateRandomKey()
 	# key=kg.generate(128)
 	key=0x2b7e151628aed2a6abf7158809cf4f3c
 	enc=AES_Encryptor.AES_Encryptor(key)
-	print_matrix(enc.getRoundkey())
-	print_matrix(enc.getRoundkey())
+	for i in range(11):
+		print_matrix(enc.getRoundkey(),i)
+
